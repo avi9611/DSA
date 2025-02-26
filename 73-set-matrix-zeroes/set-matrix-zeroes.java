0d3583e8ae492ad4cd78b2d9ff4ded1f8ea0194b
Avinash N <avinashpoojary651@gmail.com>
2025-02-26 11:17:37 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (65.50%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -3,6 +3,7 @@ class Solution {
         int m = matrix.length, n = matrix[0].length;
         boolean firstRowZero = false, firstColZero = false;
 
+        // Check if first row needs to be zero
         for(int j = 0; j < n; j++){
             if(matrix[0][j] == 0){
                 firstRowZero = true;
@@ -10,6 +11,7 @@ class Solution {
             }
         }
 
+        // Check if first column needs to be zero
         for(int i = 0; i < m; i++){
             if(matrix[i][0] == 0){
                 firstColZero = true;
@@ -17,6 +19,7 @@ class Solution {
             }
         }
 
+        // Use first row and column as markers
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
                 if(matrix[i][j] == 0){
@@ -26,6 +29,7 @@ class Solution {
             }
         }
 
+        // Set matrix cells to zero based on markers
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
                 if(matrix[0][j] == 0 || matrix[i][0] == 0){
@@ -34,12 +38,14 @@ class Solution {
             }
         }
 
+        // Set first row to zero if needed
         if(firstRowZero){
             for(int j = 0; j < n; j++){
                 matrix[0][j] = 0;
             }
         }
 
+        // Set first column to zero if needed
         if(firstColZero){
             for(int i = 0; i < m; i++){
                 matrix[i][0] = 0;

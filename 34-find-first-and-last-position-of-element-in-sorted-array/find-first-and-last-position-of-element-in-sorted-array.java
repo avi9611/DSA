a5f40328fd869614599fd71d04ef227ba85649e6
Avinash N <avinashpoojary651@gmail.com>
2025-03-07 09:37:58 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (29.45%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -5,40 +5,45 @@ class Solution {
         return new int[]{first, last};
     }
 
-    private int findFirst(int[] nums, int target){
+    // searching the first occurence method
+    private int findFirst(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         int first = -1;
-
-        while(left <= right){
+        
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-
-            if(nums[mid] == target){
-                first = mid;
-                right = mid - 1;
-            } else if(nums[mid] < target){
+            
+            if (nums[mid] == target) {
+                first = mid; 
+                right = mid - 1; // Continue searching left
+            } else if (nums[mid] < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
+        
         return first;
     }
-        private int findLast(int[] nums, int target){
-            int left = 0, right = nums.length - 1;
-            int last = -1;
 
-            while(left <= right){
-                int mid = left + (right - left) / 2;
-
-                if(nums[mid] == target){
-                    last = mid;
-                    left = mid + 1;
-                } else if(nums[mid] < target){
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+    // searching the last occurence method
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int last = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                last = mid; 
+                left = mid + 1; // Continue searching right
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-            return last;
         }
+        
+        return last;
+    }
 }

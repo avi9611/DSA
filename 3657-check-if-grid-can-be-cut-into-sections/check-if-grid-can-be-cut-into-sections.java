8d1077b762a162d42fc49a6c6e35beb6c2444683
Avinash N <avinashpoojary651@gmail.com>
2025-03-25 10:39:05 +0530

    Time: 126 ms (24.12%) | Memory: 122.2 MB (57.46%) - LeetSync

---
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.java
@@ -0,0 +1,36 @@
+class Solution {
+  public boolean checkValidCuts(int n, int[][] rectangles) {
+    int[][] xCuts = new int[rectangles.length][2];
+    int[][] yCuts = new int[rectangles.length][2];
+
+    for (int i = 0; i < rectangles.length; i++) {
+      xCuts[i][0] = rectangles[i][0];
+      xCuts[i][1] = rectangles[i][2];
+      yCuts[i][0] = rectangles[i][1];
+      yCuts[i][1] = rectangles[i][3];
+    }
+
+    return Math.max(validCuts(xCuts), validCuts(yCuts)) >= 3;
+  }
+
+// using merge intervals algorithm
+  private int validCuts(int[][] intervals) {
+    int cnt = 0;
+    int prevEnd = 0;
+
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+    for (int[] interval : intervals) {
+      final int currStart = interval[0];
+      final int currEnd = interval[1];
+      if (currStart < prevEnd) {
+        prevEnd = Math.max(prevEnd, currEnd);
+      } else {
+        prevEnd = currEnd;
+        cnt++;
+      }
+    }
+
+    return cnt;
+  }
+}

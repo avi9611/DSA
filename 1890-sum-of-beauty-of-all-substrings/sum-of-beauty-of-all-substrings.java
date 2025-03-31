0562bb894bb7e1dc62ff1ae3f9ebcebe7cdf3b3b
Avinash N <avinashpoojary651@gmail.com>
2025-03-31 16:18:25 +0530

    Time: 68 ms (43.26%) | Memory: 42.6 MB (85.66%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int beautySum(String s) {
+        int n = s.length();
+        int totalBeut = 0;
+
+        // Iterate over all starting points of substrings
+        for(int i = 0; i < n; i++){
+            int[] freq = new int[26];
+
+            // Expand the substring ending at j
+            for(int j = i; j < n; j++){
+                freq[s.charAt(j) - 'a']++;
+                // Calculate the beauty of the current substring
+                totalBeut += getBeut(freq);
+            }
+        }
+        return totalBeut;
+    }
+
+    // Helper function to calculate beauty of a substring
+    private int getBeut(int[] freq){
+        int maxFreq = 0, minFreq = Integer.MAX_VALUE;
+
+        for(int cnt: freq){
+            if(cnt > 0){
+            maxFreq = Math.max(maxFreq, cnt);
+            minFreq = Math.min(minFreq, cnt);
+            }
+        }
+        return maxFreq - minFreq;
+    }
+}

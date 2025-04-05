e00fd29ed28ea7f0db2239fbaffac46201aaefe0
Avinash N <avinashpoojary651@gmail.com>
2025-04-05 10:09:14 +0530

    Time: 1 ms (99.90%) | Memory: 48.6 MB (68.26%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,15 +11,19 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA == null || headB == null) return null;
+        // If either list is null, there's no intersection
+        if (headA == null || headB == null) return null;
 
         ListNode a = headA;
         ListNode b = headB;
 
-        while( a != b){
+        // Traverse both lists. After reaching the end of one list, continue from the other list's head.
+        while (a != b) {
             a = (a == null) ? headB : a.next;
             b = (b == null) ? headA : b.next;
         }
+
+        // Either both pointers meet at the intersection, or at null
         return a;
     }
 }

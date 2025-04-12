e54c43c4cf1eaf7573dca5e0fccdfdf6b4ab48b7
Avinash N <avinashpoojary651@gmail.com>
2025-04-12 10:15:01 +0530

    Time: 3 ms (77.03%) | Memory: 42.8 MB (99.45%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -7,13 +7,16 @@ class Solution {
     }
 
     private void backtrack (int[] candidates, int target, int start, List<Integer> current, List<List<Integer>> result){
+        // Found a valid combination
         if(target == 0){
             result.add(new ArrayList<>(current));
             return;
         }
         for(int i = start; i < candidates.length; i++){
+            // Skip duplicates
             if(i > start && candidates[i] == candidates[i - 1]) continue;
 
+            // out of bound
             if(candidates[i] > target) break;
 
             current.add(candidates[i]);

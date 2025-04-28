2e136abc6a32693289560331c3467c1788973c85
Avinash N <avinashpoojary651@gmail.com>
2025-04-28 12:13:59 +0530

    Time: 10 ms (85.29%) | Memory: 47 MB (5.46%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < 2 * n; i++){
+            // simulate circular array
+            int num = nums[i % n];
+            while(!stack.isEmpty() && nums[stack.peek()] < num){
+                res[stack.pop()] = num;
+            }
+            if(i < n){
+                stack.push(i);
+            }
+        }
+        return res;
+    }
+}

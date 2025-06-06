c913dba8573f3e3ee56b1dfbac10df16cf184313
Avinash N <avinashpoojary651@gmail.com>
2025-06-06 09:47:41 +0530

    Time: 1 ms (79.55%) | Memory: 41.6 MB (32.12%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        
+        // If lengths don't match, it's not a bijection
+        if (words.length != pattern.length()) {
+            return false;
+        }
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            // Check if mapping already exists
+            if (charToWord.containsKey(c)) {
+                if (!charToWord.get(c).equals(word)) {
+                    return false;
+                }
+            } else {
+                charToWord.put(c, word);
+            }
+
+            // Check reverse mapping
+            if (wordToChar.containsKey(word)) {
+                if (wordToChar.get(word) != c) {
+                    return false;
+                }
+            } else {
+                wordToChar.put(word, c);
+            }
+        }
+
+        return true;
+    }
+}

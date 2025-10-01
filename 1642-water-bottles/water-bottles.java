0c22209dfcadce1b9c1f77c67d89c60db9756197
Avinash N <avinashpoojary651@gmail.com>
2025-10-01 09:42:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (86.01%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int drunk = numBottles;
+        int empty = numBottles;
+
+        while(empty >= numExchange){
+            int newBottles = empty / numExchange;
+            drunk += newBottles;
+            empty = (empty % numExchange) + newBottles;
+        }
+        return drunk;
+    }
+}

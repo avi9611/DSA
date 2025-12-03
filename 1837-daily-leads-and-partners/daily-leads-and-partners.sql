e402ec4573207b9c78ec5283c26c28bbbb8e1b1b
Avinash N <avinashpoojary651@gmail.com>
2025-12-03 21:25:02 +0530

    Time: 671 ms (15.15%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1837-daily-leads-and-partners/daily-leads-and-partners.sql
@@ -0,0 +1,9 @@
+/* Write your PL/SQL query statement below */
+
+SELECT 
+    TO_CHAR(date_id, 'YYYY-MM-DD') AS date_id,
+    make_name,
+    COUNT(DISTINCT lead_id) AS unique_leads,
+    COUNT(DISTINCT partner_id) AS unique_partners
+FROM DailySales
+GROUP BY date_id, make_name;

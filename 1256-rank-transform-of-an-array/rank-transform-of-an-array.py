b9496e101ef863a38fe0cfcab3956b10e261369e
Avinash N <avinashpoojary651@gmail.com>
2026-01-04 10:47:10 +0530

    Time: 43 ms (43.58%) | Memory: 35.4 MB (85.65%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -1,52 +1,14 @@
+from typing import List
+
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        n = len(arr)
-        if n == 0:
-            return []
+        sorted_arr = sorted(arr)
 
-        def merge_sort(a):
-            if len(a) <= 1:
-                return a
-
-            mid = len(a) // 2
-            left = merge_sort(a[:mid])
-            right = merge_sort(a[mid:])
-
-            return merge(left, right)
-
-        def merge(left, right):
-            i = j = 0
-            res = []
-
-            while i < len(left) and j < len(right):
-                if left[i] <= right[j]:
-                    res.append(left[i])
-                    i += 1
-                else:
-                    res.append(right[j])
-                    j += 1
-
-            while i < len(left):
-                res.append(left[i])
-                i += 1
-
-            while j < len(right):
-                res.append(right[j])
-                j += 1
-
-            return res
-        sort_arr = [0] * n
-        for i in range(n):
-            sort_arr[i] = arr[i]
-        sort_arr = merge_sort(sort_arr)
         rank_map = {}
         rank = 1
-        for i in range(n):
-            if sort_arr[i] not in rank_map:
-                rank_map[sort_arr[i]] = rank
-                rank += 1
-        res = [0] * n
-        for i in range(n):
-            res[i] = rank_map[arr[i]]
 
-        return res
+        for num in sorted_arr:
+            if num not in rank_map:
+                rank_map[num] = rank
+                rank += 1
+        return [rank_map[num] for num in arr]
